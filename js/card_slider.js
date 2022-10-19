d29fc43f4eba836f383b30548ae8bf316290eddc
VetalChess07 <103288446+VetalChess07@users.noreply.github.com>
2022-10-19 03:10:37 +0300

    third commit

---
+++ js/card_slider.js
@@ -0,0 +1,28 @@
+var swiper_card = new Swiper(".mySwiper1", {
+   effect: "cards",
+   grabCursor: true,
+});
+
+let image = document.querySelectorAll('*[data-modal-img]');
+
+for (let i = 0; i < image.length; i++) {
+   image[i].addEventListener('click', function () {
+      let name = image[i].getAttribute('data-modal-img');
+      let modal = document.querySelector("[data-modal-window='" + name + "']");
+      modal.style.display = "block";
+      let close = modal.querySelector(".close_modal_window");
+      close.addEventListener('click', function () {
+         modal.style.display = "none";
+      })
+   })
+}
+
+window.onclick = function (e) {
+   if (e.target.hasAttribute('data-modal-window')) {
+      let modals = document.querySelectorAll("*[data-modal-window]")
+      for (let i = 0; i < modals.length; i++) {
+         modals[i].style.display = "none";
+      }
+
+   }
+}
