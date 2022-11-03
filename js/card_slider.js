cb87017ada4beff50c929f83750d06c298266f39
VetalChess07 <103288446+VetalChess07@users.noreply.github.com>
2022-11-03 20:48:19 +0300

    preparation for cms

--- js/card_slider.js
+++ js/card_slider.js
@@ -101,40 +101,40 @@ burgerBtn.addEventListener('click', (e) => {
    // burgerItem.classList.toggle("class");
 });
 
-const header = document.querySelector('.header');
-const first = document.querySelector('.gallery');
+// const header = document.querySelector('.header');
+// const first = document.querySelector('.gallery');
 
-const headerHeight = header.offsetHeight;
-const firstHeight = first.offsetHeight;
-let lastScrollTop = 0;
-console.log(header)
-console.log(first)
-console.log(headerHeight)
-console.log(firstHeight)
+// const headerHeight = header.offsetHeight;
+// const firstHeight = first.offsetHeight;
+// let lastScrollTop = 0;
+// console.log(header)
+// console.log(first)
+// console.log(headerHeight)
+// console.log(firstHeight)
 
-window.addEventListener('scroll', () => {
-   let scrollDistance = window.scrollY;
+// window.addEventListener('scroll', () => {
+//    let scrollDistance = window.scrollY;
 
-   // if (scrollDistance >= firstHeight) {
-   //    header.classList.add('fixed');
-   //    first.style.marginTop = `${headerHeight}px`;
-   // } else {
-   //    header.classList.remove('fixed');
-   //    first.style.marginTop = null;
-   // }
+//    if (scrollDistance >= firstHeight) {
+//       header.classList.add('fixed');
+//       first.style.marginTop = `${headerHeight}px`;
+//    } else {
+//       header.classList.remove('fixed');
+//       first.style.marginTop = null;
+//    }
 
-   if (scrollDistance > lastScrollTop) {
-      header.classList.remove('fixed');
-      first.style.marginTop = null;
-   } else {
-      header.classList.add('fixed');
-      first.style.marginTop = `${headerHeight}px`;
-   }
+//    if (scrollDistance >= lastScrollTop) {
+//       header.classList.remove('fixed');
+//       first.style.marginTop = null;
+//    } else {
+//       header.classList.add('fixed');
+//       first.style.marginTop = `${headerHeight}px`;
+//    }
 
-   if (scrollDistance === 0) {
-      header.classList.remove('fixed');
-      first.style.marginTop = null;
-   }
+//    if (scrollDistance === 0) {
+//       header.classList.remove('fixed');
+//       first.style.marginTop = null;
+//    }
 
-   lastScrollTop = scrollDistance;
-});
+//    lastScrollTop = scrollDistance;
+// });
