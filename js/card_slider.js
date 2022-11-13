1ff414cc972122844afa54df94c8d72afdbaeeaa
VetalChess07 <103288446+VetalChess07@users.noreply.github.com>
2022-11-13 20:19:41 +0300

    reboot part 1

--- js/card_slider.js
+++ js/card_slider.js
@@ -1,93 +1,108 @@
-let swiper_card = new Swiper(".mySwiper1", {
-   effect: "cards",
-   grabCursor: true,
-});
+let swiper_card = new Swiper(".swiper ", {
+   loop: true,
 
-btnOff = document.querySelectorAll('.swiper-slide__card'),
-   btnOn = document.querySelectorAll('.close-popup'),
-   body = document.body;
+   // If we need pagination
+   // pagination: {
+   //    el: '.swiper-pagination',
+   // },
 
-btnOff.forEach(b => b.addEventListener('click', (b) => {
-   disableScroll();
-   // b.currentTarget.style.pointerEvents = 'none';
-   // btnOn.style.pointerEvents = 'auto';
-}));
-
-btnOn.forEach(b => b.addEventListener('click', (b) => {
-   enableScroll();
-   // b.currentTarget.style.pointerEvents = 'none';
-   // btnOff.style.pointerEvents = 'auto';
-}));
-
-let disableScroll = function () {
-   let pagePosition = window.scrollY;
-   document.body.classList.add('active');
-   document.body.dataset.position = pagePosition;
-   document.body.style.top = -pagePosition + 'px';
-}
-
-
-let enableScroll = function () {
-   let pagePosition = parseInt(document.body.dataset.position, 10);
-   document.body.style.top = 'auto';
-   document.body.classList.remove('active');
-   window.scroll({ top: pagePosition, left: 0 });
-   document.body.removeAttribute('active');
-}
-
-
-
-
-let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
-let popup = document.querySelector('.popup'); // Само окно
-let openPopupButtons = document.querySelectorAll('.swiper-slide__card'); // Кнопки для показа окна
-let closePopupButton = document.querySelector('.close-popup');
-// let body = document.body;
-// console.log(body)
-
-
-
-
-openPopupButtons.forEach((button) => { // Перебираем все кнопки
-   button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-      popup.classList.add('active'); // И для самого окна
-      body.classList.add('active')
-
-   })
-});
-
-closePopupButton.addEventListener('click', () => { // Вешаем обработчик на крестик
-   popupBg.classList.remove('active'); // Убираем активный класс с фона
-   popup.classList.remove('active'); // И с окна
-   body.classList.remove('active');
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+   pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets'
+   },
 
 });
 
-document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-   if (e.target === popupBg) { // Если цель клика - фот, то:
-      popupBg.classList.remove('active'); // Убираем активный класс с фона
-      popup.classList.remove('active'); // И с окна
-      body.classList.remove('active');
+// btnOff = document.querySelectorAll('.swiper-slide__card'),
+//    btnOn = document.querySelectorAll('.close-popup'),
+//    body = document.body;
 
-   }
-});
+// btnOff.forEach(b => b.addEventListener('click', (b) => {
+//    disableScroll();
+//    // b.currentTarget.style.pointerEvents = 'none';
+//    // btnOn.style.pointerEvents = 'auto';
+// }));
 
-cards = document.querySelectorAll(".swiper-slide__card");
-cards.forEach((card) => {
-   card.addEventListener("click", (e) => {
-      let targetEl = e.target;
-      let targetCardTitle = card.querySelector(".card__title");
-      let targetCardImage = card.querySelector(".card__image");
-      let targetCardText = card.querySelector(".card__text");
+// btnOn.forEach(b => b.addEventListener('click', (b) => {
+//    enableScroll();
+//    // b.currentTarget.style.pointerEvents = 'none';
+//    // btnOff.style.pointerEvents = 'auto';
+// }));
 
-      document.querySelector(".popup__title").textContent = targetCardTitle.textContent;
-      document.querySelector(".popup__text").textContent = targetCardText.textContent;
-      document.querySelector(".popup__image").src = targetCardImage.src;
+// let disableScroll = function () {
+//    let pagePosition = window.scrollY;
+//    document.body.classList.add('active');
+//    document.body.dataset.position = pagePosition;
+//    document.body.style.top = -pagePosition + 'px';
+// }
 
-   });
-});
+
+// let enableScroll = function () {
+//    let pagePosition = parseInt(document.body.dataset.position, 10);
+//    document.body.style.top = 'auto';
+//    document.body.classList.remove('active');
+//    window.scroll({ top: pagePosition, left: 0 });
+//    document.body.removeAttribute('active');
+// }
+
+
+
+
+// let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+// let popup = document.querySelector('.popup'); // Само окно
+// let openPopupButtons = document.querySelectorAll('.swiper-slide__card'); // Кнопки для показа окна
+// let closePopupButton = document.querySelector('.close-popup');
+// // let body = document.body;
+// // console.log(body)
+
+
+
+
+// openPopupButtons.forEach((button) => { // Перебираем все кнопки
+//    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+//       e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+//       popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+//       popup.classList.add('active'); // И для самого окна
+//       body.classList.add('active')
+
+//    })
+// });
+
+// closePopupButton.addEventListener('click', () => { // Вешаем обработчик на крестик
+//    popupBg.classList.remove('active'); // Убираем активный класс с фона
+//    popup.classList.remove('active'); // И с окна
+//    body.classList.remove('active');
+
+// });
+
+// document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+//    if (e.target === popupBg) { // Если цель клика - фот, то:
+//       popupBg.classList.remove('active'); // Убираем активный класс с фона
+//       popup.classList.remove('active'); // И с окна
+//       body.classList.remove('active');
+
+//    }
+// });
+
+// cards = document.querySelectorAll(".swiper-slide__card");
+// cards.forEach((card) => {
+//    card.addEventListener("click", (e) => {
+//       let targetEl = e.target;
+//       let targetCardTitle = card.querySelector(".card__title");
+//       let targetCardImage = card.querySelector(".card__image");
+//       let targetCardText = card.querySelector(".card__text");
+
+//       document.querySelector(".popup__title").textContent = targetCardTitle.textContent;
+//       document.querySelector(".popup__text").textContent = targetCardText.textContent;
+//       document.querySelector(".popup__image").src = targetCardImage.src;
+
+//    });
+// });
 
 let burgerBtn = document.querySelector(".burger-btn")
 
